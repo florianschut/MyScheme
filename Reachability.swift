2a07f839d025cca6fb2728db27733da1207076b7
florianschut <Florian@iMac.local>
2016-03-10 17:32:41 +0100

    File management
    Added/fucked some files

---
+++ Reachability.swift
@@ -0,0 +1,29 @@
+//
+//  Reachability.swift
+//  MyScheme
+//
+// As found on: http://stackoverflow.com/questions/30743408/check-for-internet-connection-in-swift-2-ios-9
+//
+//
+
+import Foundation
+import SystemConfiguration
+
+public class Reachability {
+    //Checks for internet connection and returns true or false
+    class func isConnectedToNetwork() -> Bool {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
+            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
+        }
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        return (isReachable && !needsConnection)
+    }
+}
